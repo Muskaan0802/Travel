cee3dbe89fad556fc0abd6fb6d503060544ec6bc
Muskaan0802 <muskaan020516@gmail.com>
2024-03-18 14:24:29 +0530

    script.js

--- script.js
+++ script.js
@@ -6,3 +6,51 @@ menu.onclick = () =>{
     navbar.classList.toggle('active');
 
 }
+Window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+ var swiper = new Swiper(".home-slider", {
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+//1:00:00
+  var swiper = new Swiper(".reviews-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoHeight:true,
+    grabCursor:true,
+    navigation: {
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      
+    },
+    768: {
+      slidesPerView: 2,
+      
+    },
+    1024: {
+      slidesPerView: 3,
+     
+   } },
+  },
+});
+
+
+let loadMoreBtn = document.querySelector('.package .load-more .btn');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () => {
+  let boxes = [...document.querySelectorAll('.package .box-container .box')];
+  for(var i = currentItem;i <currentItem + 3; i++){
+    boxes[i].style.display = 'inline-block';
+  };
+  currentItem += 3;
+  if(currentItem >= boxes.length){
+    loadMoreBtn.style.display = 'none';
+  }
+};
